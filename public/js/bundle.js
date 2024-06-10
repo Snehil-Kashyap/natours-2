71451dc7c4305742e3ae20a0074f6844319838f6
Snehil-Kashyap <snehilk31@gmail.com>
2024-06-11 02:13:45 +0530

    bundler fix

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12463,7 +12463,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64199" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64222" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
